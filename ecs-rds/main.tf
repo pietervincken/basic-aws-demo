ba9862751db9484184922f9e006e53b6d2e64692
pietervincken <4153479+pietervincken@users.noreply.github.com>
2023-12-03 11:54:54 +0100

    feat: start with ECS RDS demo

---
+++ ecs-rds/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=5.24.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      owner      = "basic-aws-demo"
+      created-by = "pieter.vincken@ordina.be"
+    }
+  }
+}
+
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  tags = {
+    owner      = "basic-aws-demo"
+    created-by = "pieter.vincken@ordina.be"
+  }
+
+  name = "basicawsdemo"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs = [
+    data.aws_availability_zones.available.zone_ids[0],
+    data.aws_availability_zones.available.zone_ids[1]
+  ]
+
+  container_port = 8080
+}
---
+++ ecs-rds/output.tf
@@ -0,0 +1,3 @@
+output "alb_fqdn" {
+  value = module.alb.dns_name
+}
